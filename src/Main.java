1e46cd371269d063e51ddb9804a67ce4016fed80
SheldonPeng <775887734@qq.comgit config --global user.email  775887734@qq.com>
2019-07-06 22:00:40 +0800

    "sadsadsa"

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("安其拉大叔大婶百度金矿芭思蔻");
         System.out.println("掉头发了掉头发了");
         System.out.println("华琛没头发了！");
+        System.err.println("1211231313131231231");
     }
 }
