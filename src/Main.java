d71090a52fec4c2309776be4fe5009d1bdc37bd6
SheldonPeng <775887734@qq.comgit config --global user.email  775887734@qq.com>
2019-07-06 21:24:26 +0800

    collision ssss

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         System.out.println("加需求掉头发");
         System.out.println("需求就是因为掉了头发才加的");
-        System.out.println("冲突测试测试测试测按到按到");
+        System.out.println("安其拉大叔大婶百度金矿芭思蔻");
         System.out.println("掉头发了掉头发了");
         System.out.println("华琛没头发了！");
     }
