62eac5310cc6de3f1efdb37fd0cfacff06a4e82b
huange7 <1048461654@qq.com>
2019-07-06 20:07:41 +0800

    first

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("掉头发就是因为加需求");
         System.out.println("长胖的唯一祸源就是因为给师弟加了需求!");
         System.out.println("掉头发了掉头发了");
+        System.out.println("我是光头了没头发啦！");
     }
 }
